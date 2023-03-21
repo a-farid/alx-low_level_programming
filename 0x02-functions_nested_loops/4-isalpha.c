b8f16e4c6e2d0ce1c4e7ea0a7a7ae23b535ed0c8
farid <fahizoune10@gmail.com>
2023-03-21 15:45:05 -0700

    tasks

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - Check main
+ * @c: An input character
+ * Description: Returning 1 if the character is a
+ * letter, lowercase or uppercase.
+ * Return: 1 or 0 in otherwise.
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+	int isletter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				isletter = 1;
+		}
+	}
+	return (isletter);
+}
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Check main
+ * @r: An integer input
+ * Description: Printing the last digit of a given number
+ * Return: last digit of number
+ */
+
+int print_last_digit(int r)
+{
+	int n;
+
+	if (r < 0)a
+		n = -1 * (r % 10);
+	else
+		n = r % 10;
+
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
