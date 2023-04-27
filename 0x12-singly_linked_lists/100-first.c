90fc844a6ed04194cbea74e84f2c617ab544d40e
farid <fahizoune10@gmail.com>
2023-04-27 15:47:28 -0700

    linked lists tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * bmain - function executed before the main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
