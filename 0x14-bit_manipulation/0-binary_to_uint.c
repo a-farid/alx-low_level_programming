69a185cfae375dfdbb478f92c71db41564994eef
faridlord10 <fahizoune10@gmail.com>
2023-07-06 15:19:41 -0700

    add error handler

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,10 +6,21 @@
  * Return: the converted number.
  */
 unsigned int binary_to_uint(const char *b){
-  unsigned int result = 0;
-  int x = 0;
-  while(b[x] != '\0') {
-    result = (result << 1) + (b[x] - '0');
+    if(!b){
+        return 0;
+    }
+  
+    unsigned int result = 0;
+    int x = 0;
+  
+    while(b[x] != '\0') {
+    
+        if (b[x] != '0' && b[x] != '1')
+        {
+            return (0);
+        }
+
+        result = (result << 1) + (b[x] - '0');
         x++;
   }
   return result;
