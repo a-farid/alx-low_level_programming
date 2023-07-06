adef3831165705e3a37543c97c6e30af17013413
faridlord10 <fahizoune10@gmail.com>
2023-07-06 14:42:16 -0700

    first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b : binary.
+ * Return: the converted number.
+ */
+unsigned int binary_to_uint(const char *b){
+  unsigned int result = 0;
+  int x = 0;
+  while(b[x] != '\0') {
+    result = (result << 1) + (b[x] - '0');
+        x++;
+  }
+  return result;
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Bitwise manipilations in C language
