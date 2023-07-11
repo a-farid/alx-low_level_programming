22b0dec889c7a61e2b8ae7ee5588d00d7ec22925
faridlord10 <fahizoune10@gmail.com>
2023-07-10 17:18:45 -0700

    Task number 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file == -1)
+	
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (len = 0; text_content[len]; len++)
-		;
+	for (len = 0; text_content[len]; len++);
 
 	Wr = write(fd, text_content, len);
 	if (Wr == -1)
